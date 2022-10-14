dbd7caace9d0e6f13a006c9b64e83cb277229b8f
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2022-10-14 17:01:57 -0300

    accounts as a config client

--- accounts/src/main/java/com/eazybytes/accounts/controllers/AccountController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controllers/AccountController.java
@@ -1,14 +1,20 @@
 package com.eazybytes.accounts.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eazybytes.accounts.config.AccountsService;
 import com.eazybytes.accounts.models.Accounts;
 import com.eazybytes.accounts.models.Customer;
+import com.eazybytes.accounts.models.Properties;
 import com.eazybytes.accounts.repositories.AccountRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import io.micrometer.core.annotation.Timed;
 
@@ -17,6 +23,9 @@ import io.micrometer.core.annotation.Timed;
 public class AccountController {
 	@Autowired
 	private AccountRepository repository;
+	
+	@Autowired
+	private AccountsService accountsConfig;
 	
 	@PostMapping("/myAccount")
 	@Timed(value="getAccountDetails.time", description="time taken to return account details")
@@ -29,4 +38,23 @@ public class AccountController {
 			return null;
 		}
 	}
+	
+	@GetMapping("/accounts/properties")
+	public String getPropertyDetails() throws JsonProcessingException{
+		ObjectWriter ow=new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties=new Properties(accountsConfig.getMsg(),accountsConfig.getBuildVersion(), 
+				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+		String jsonStr=ow.writeValueAsString(properties);
+		return jsonStr;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
