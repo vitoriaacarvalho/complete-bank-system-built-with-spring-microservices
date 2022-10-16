30d4993074c356b689489708bcf7cfbf3b2182f1
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2022-10-16 09:27:40 -0300

    cards microservice as a client config

--- cards/src/main/java/com/eazybytes/CardsApplication.java
+++ cards/src/main/java/com/eazybytes/CardsApplication.java
@@ -3,11 +3,13 @@ package com.eazybytes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScans({@ComponentScan("com.eazybytes.controllers")})
+@RefreshScope
 @EnableJpaRepositories("com.eazybytes.repositories")
 @EntityScan("com.eazybytes.models")
 @SpringBootApplication
--- cards/src/main/java/com/eazybytes/controller/CardController.java
+++ cards/src/main/java/com/eazybytes/controller/CardController.java
@@ -3,19 +3,29 @@ package com.eazybytes.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eazybytes.config.CardsService;
 import com.eazybytes.models.Cards;
 import com.eazybytes.models.Customer;
+import com.eazybytes.models.Properties;
 import com.eazybytes.repositories.CardRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class CardController {
 	
 	@Autowired
 	private CardRepository repo;
+	
+	@Autowired
+	private CardsService cardsConfig;
+	
 	
 	@PostMapping("/myCards")
 	public List<Cards> getCardDetails(@RequestBody Customer customer){
@@ -27,4 +37,20 @@ public class CardController {
 			return null;
 		}
 	}
+	
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException{
+		ObjectWriter ow= new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties=new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(), cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+		String jsonStr= ow.writeValueAsString(properties);
+		return jsonStr;
+}
+	
+	
+	
+	
+	
+	
+	
+	
 }
